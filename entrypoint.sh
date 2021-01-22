e4d1355d8c642ad881a6f11df34b3c85e816adfc
Will Salemé <will@blauqs.com>
2021-01-22 13:13:23 -0700

    Added extensive error handling to make debugging straight forward

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,53 +1,145 @@
 #!/usr/bin/env bash
 
+#
+# Some helpful helpers
+#
+
+function errecho() {
+    >&2 echo -e "\033[0;31mERR: ${*}\033[0m"
+}
+
+#
+# Declare Error Codes
+#
+
+readonly ERR_GEN=1
+readonly ERR_MISS_REPO=2
+readonly ERR_NO_RELEASES=3
+readonly ERR_NO_ASSET=4
+
+#
+# Parse Action Inputs
+#
+
 # Ensure we are given the file name
 if [[ -z "${INPUT_FILE}" ]]; then
-  echo "Missing Required Input: 'file'"
-  exit 1
+  errecho "Missing Required Input: 'file'"
+  exit "${ERR_GEN}"
 fi
 
-# Resolve the repo owner/name path
-REPO=
-if [[ -z "${INPUT_REPO}" ]]; then
-  if [[ -z "${GITHUB_REPOSITORY}" && -z "${GITHUB_REPOSITORY_OWNER}" ]]; then
-    echo "Missing repository location, either GitHub should provide it or the 'repo' Input"
-    exit 2
+# Resolve the owner/repo repository path
+REPO="${INPUT_REPO}"
+if [[ -z "${REPO}" ]]; then
+  if [[ -z "${GITHUB_REPOSITORY}" ]]; then
+    errecho "Missing repository location, either GitHub should provide it or the 'repo' Input"
+    exit "${ERR_MISS_REPO}"
   fi
-  REPO="${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY}"
-else
-  REPO="${INPUT_REPO}"
+  REPO="${GITHUB_REPOSITORY}"
+fi
+
+# Optional release version
+VER="${INPUT_VERSION}"
+if [[ -z "${VER}" ]]; then
+  VER="latest"
+fi
+
+# Optional tag prefix
+PREFIX="${INPUT_PREFIX}"
+if [[ -z "${PREFIX}" ]]; then
+  VER="v"
 fi
 
 # Optional access token for private repositories
-TOKEN="${GITHUB_TOKEN}"
-if [[ -n "${INPUT_TOKEN}" ]]; then
-  TOKEN="${INPUT_TOKEN}"
+TOKEN="${INPUT_TOKEN}"
+if [[ -z "${TOKEN}" ]]; then
+  TOKEN="${GITHUB_TOKEN}"
 fi
 
-# Define useful variables to be reused
-API_URL="https://${TOKEN}:@api.github.com/repos/${REPO}"
-RELEASES=$(curl "${API_URL}/releases")
-CHOSEN_RELEASE=$(echo "${RELEASES}" | jq -r ".[] | select(.tag_name | startswith(\"${INPUT_PREFIX}\"))")
-ASSET_ID=$(echo "${CHOSEN_RELEASE}" | jq -r ".assets | map(select(.name == \"${INPUT_FILE}\"))[0].id")
-TAG_VERSION=$(echo "${CHOSEN_RELEASE}" | jq -r ".tag_name" | awk -F'/' '{ print $NF }' | sed -e "s/^v//" | sed -e "s/^v.//")
+# Optional out directory
 OUT_DIR="${INPUT_OUT:-/tmp}"
 OUT_DIR="$(echo "${OUT_DIR}" | sed -E 's/\/+$//')"
 
-# Fail if we couldn't find a release
-if [[ -z "${ASSET_ID}" ]]; then
-  echo "Could not find asset id"
-  exit 3
+#
+# Call the GitHub API and Parse Responses
+#
+
+# Define the base API URL
+API_URL="https://${TOKEN}:@api.github.com/repos/${REPO}"
+
+# Fetch all of the releases in the repository
+RELEASES=$(curl -s "${API_URL}/releases")
+
+# Ensure we found a valid repo
+if [[ "$(echo "${RELEASES}" | jq -r "if type == \"object\" then .message else empty end")" == "Not Found" ]]; then
+  errecho "no repository found using the location '${REPO}'"
+  exit "${ERR_MISS_REPO}"
 fi
 
-# Download the asset file
+# Ensure we got a list of releases
+if [[ "$(echo "${RELEASES}" | jq -r "length")" -eq 0 ]]; then
+  errecho "no releases created in repo '${REPO}'"
+  exit "${ERR_NO_RELEASES}"
+fi
+
+# Choose the release, based on tag prefix and version
+CHOSEN_RELEASE=
+if [[ "${VER}" == "latest" ]]; then
+  FILTERED_RELEASES=$(echo "${RELEASES}" | jq -r "map(select(.tag_name | startswith(\"${PREFIX}\")))")
+  if [[ "$(echo "${FILTERED_RELEASES}" | jq -r "length")" -eq 0 ]]; then
+    errecho "could not find a release with the prefix '${PREFIX}'"
+    exit "${ERR_NO_RELEASES}"
+  fi
+
+  CHOSEN_RELEASE=$(echo "${FILTERED_RELEASES}" | jq -r ".[0]")
+  if [[ -z "${CHOSEN_RELEASE}" || "${CHOSEN_RELEASE}" == "null" ]]; then
+    errecho "could not find a release with the prefix '${PREFIX}'"
+    exit "${ERR_NO_RELEASES}"
+  fi
+else
+  FILTERED_RELEASES=$(echo "${RELEASES}" | jq -r "map(select(.tag_name == \"${PREFIX}${VER}\"))")
+  if [[ "$(echo "${FILTERED_RELEASES}" | jq -r "length")" -eq 0 ]]; then
+    errecho "could not find a release with the prefix/version '${PREFIX}${VER}'"
+    exit "${ERR_NO_RELEASES}"
+  fi
+
+  CHOSEN_RELEASE=$(echo "${FILTERED_RELEASES}" | jq -r ".[0]")
+  if [[ -z "${CHOSEN_RELEASE}" || "${CHOSEN_RELEASE}" == "null" ]]; then
+    errecho "could not find a release with the prefix/version '${PREFIX}${VER}'"
+    exit "${ERR_NO_RELEASES}"
+  fi
+fi
+
+# Choose the release asset, based on the given file name
+ASSET=$(echo "${CHOSEN_RELEASE}" | jq -r ".assets | map(select(.name == \"${INPUT_FILE}\"))")
+if [[ "$(echo "${ASSET}" | jq -r "length")" -eq 0 ]]; then
+  errecho "could not find an asset with the file name '${INPUT_FILE}'"
+  exit "${ERR_NO_ASSET}"
+fi
+
+ASSET_ID=$(echo "${ASSET}" | jq -r ".[0] | .id")
+if [[ -z "${ASSET_ID}" ]]; then
+  errecho "could not get the asset id with the file name '${INPUT_FILE}'"
+  exit "${ERR_NO_ASSET}"
+fi
+
+# Get the release tag name, in case the "latest" version was used
+TAG_VERSION=$(echo "${CHOSEN_RELEASE}" | jq -r ".tag_name" | awk -F'/' '{ print $NF }' | sed -e "s/^v//" | sed -e "s/^v.//")
+
+#
+# Download the resolved Asset ID from GitHub Release
+#
+
 curl \
   -s \
   -J \
   -L \
   -H "Accept: application/octet-stream" \
-  "${API_URL}/releases/assets/${ASSET_ID}" \
-  -o "${OUT_DIR}/${INPUT_FILE}"
+  -o "${OUT_DIR}/${INPUT_FILE}" \
+  "${API_URL}/releases/assets/${ASSET_ID}"
 
-# Declare the output values for Workflow to read
+#
+# Declare the Workflow Outputs
+#
+
 echo "##[set-output name=out;]${OUT_DIR}/${INPUT_FILE}"
 echo "##[set-output name=version;]${TAG_VERSION}"
